3d7a93dd6c6c1ba9e81664ff70b1fc12c4925363
Dennis Fransen <dennis.fransen@trinax.se>
2024-02-22 20:28:17 +0100

    Added rose-pine theme in nvim. Downloaded rose-pine them for alacritty but not using it

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
-	{ "ellisonleao/gruvbox.nvim" },
-	{ "sainnhe/sonokai" },
-	{ "sainnhe/edge" },
-	{ "sainnhe/everforest" },
-	{ "nordtheme/vim" },
+	{ "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+	{ "sainnhe/sonokai", name = "sonokai" },
+	{ "sainnhe/edge", name = "edge" },
+	{ "sainnhe/everforest", name = "everforest" },
+	{ "bluz71/vim-moonfly-colors", name = "moonfly" },
+	{ "rose-pine/neovim", name = "rose-pine" },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "everforest",
+			colorscheme = "rose-pine",
 		},
 	},
 }
